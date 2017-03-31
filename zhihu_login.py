309f415316e2fbdf49df99c2cf9cf1ed04d47e10
Django-27 <nfs_fly@outlook.com>
2017-03-31 09:19:45 +0800

    succeed in 20170231

--- zhihu_login.py
+++ zhihu_login.py
@@ -7,9 +7,9 @@ import cv2
 def get_captcha_1(data):
     with open('captcha.gif', 'wb') as fp:
         fp.write(data)
-    img = cv2.imread('captcha.jpg')
-    cv2.imshow('验证码', img)
-    cv2.waitKey(5)
+    # img = cv2.imread('captcha.jpg')
+    # cv2.imshow('验证码', img)
+    # cv2.waitKey(5)
     return input("输入验证码：")
 
 
@@ -30,8 +30,14 @@ def login(phone_num, password):
         'phone_num': phone_num
     }
 
+    # resp = sessiona.post('https://www.zhihu.com/login/email',data,headers=headers).content
+    # print(resp)
+    # return resp
+
     resp = session.post('https://www.zhihu.com/login/phone_num', data=data, headers=headers)  # 登录成功
     print(eval(resp.content)['msg'])
+
+
 
 if __name__ == '__main__':
     login('18614035475', '147850096')
