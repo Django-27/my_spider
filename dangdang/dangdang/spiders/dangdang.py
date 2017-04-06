4f41008af7466b3a1a32953e8ec03f2ac3c1e176
Django-27 <nfs_fly@outlook.com>
2017-04-06 20:19:55 +0800

    succeed in 20170406 去除了无用的try...exception... 修改之前的 多级 parse 间参数传递，之前是自己构造了字典，每次讲要传递的参数添加入字典， 然后在 scrapy.Request 的 属性 meta 中带上这个字典，但是会出现 KeyError 错误 改进是使用，在 parse 中就初始化 Item 对象，然后每次传递 item 实例
    多次启动 scrapy 后出现 爬虫直接 finish ，采用 scrapy.Request 中带上 dont_filteer=True 解决

--- dangdang/dangdang/spiders/dangdang.py
+++ dangdang/dangdang/spiders/dangdang.py
@@ -18,63 +18,53 @@ class DangdangSpider(scrapy.Spider):
     def start_requests(self):
         ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0'
         headers = {'User-Agent': ua}
-        yield scrapy.Request(url=self.start_urls, headers=headers, method='GET', callback=self.parse)
+        yield scrapy.Request(url=self.start_urls, headers=headers, method='GET', callback=self.parse, dont_filter=True)
 
     def parse(self, response):
+        item = DangdangItem()
         ua = 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0'
         headers = {'User-Agent': ua}
         goodslist = response.xpath('//div[@class="sort_box"]//ul/li').extract()
         for goods in goodslist:
-            try:
-                goods = html.fromstring(goods)
-                category_big_name = goods.xpath('a/@title')[0]  # 大种类, 共53个
-                category_big_url = goods.xpath('./a/@href')[0]
-                yield scrapy.Request(url=response.urljoin(category_big_url),
-                                     headers=headers, callback=self.detail_parse,
-                                     meta={"category_big_name": category_big_name})
-            except Exception:
-                pass
+            goods = html.fromstring(goods)
+            item['category_big_name'] = goods.xpath('a/@title')[0]  # 大种类, 共53个
+            category_big_url = goods.xpath('./a/@href')[0]
+            yield scrapy.Request(url=response.urljoin(category_big_url),
+                                 headers=headers, callback=self.detail_parse,
+                                 meta={'item': item}, dont_filter=True)
 
     def detail_parse(self, response):
         '''
         http://category.dangdang.com/cp01.47.00.00.00.00.html#ddclick?act=clickcat&pos=0_0_0_p&cat=01.25.00.00.00.00&key=&qinfo=&pinfo=131429_1_60&minfo=&ninfo=&custid=&permid=&ref=&rcount=&type=&t=1491304>
         '''
+        item = response.meta['item']
         goodslist = response.xpath('//div[@class="link max"]/span').extract()
         for goods in goodslist:
-            try:
-                goods = html.fromstring(goods)
-                category_small_name = goods.xpath('a/@title')[0]
-                category_small_url = goods.xpath('a/@href')[0]   # 小种类，不确定
-                yield scrapy.Request(url=response.urljoin(category_small_url),
-                                     callback=self.third_parse,
-                                     meta={"category_small_name": category_small_name,
-                                           "category_big_name": response.meta['category_big_name']})
-            except Exception:
-                pass
+            goods = html.fromstring(goods)
+            item['category_small_name'] = goods.xpath('a/@title')[0]
+            category_small_url = goods.xpath('a/@href')[0]   # 小种类，不确定
+            yield scrapy.Request(url=response.urljoin(category_small_url),
+                                 callback=self.third_parse,
+                                 meta={'item': item}, dont_filter=True)
 
     def third_parse(self, response):
         '''
         http://category.dangdang.com/cp01.47.02.00.00.00.html#ddclick?act=clickcat&pos=0_0_0_p&cat=01.47.00.00.00.00&key=&qinfo=&pinfo=1021302_1_60&minfo=&ninfo=&custid=&permid=&ref=&rcount=&type=&t=1491375720000&sell_run=0&searchapi_version=eb_split'
         '''
-        item = DangdangItem()
         for _ in range(3):
+            item = response.meta['item']
             books = response.xpath('//div[@class="con shoplist"]/ul/li')  # 正常一页60项
             for book in books:
-                book_price_cn = book.css('p.price span.price_n::text').extract_first()
-                book_author = book.css('p.author a::text').extract_first()
-                book_name = book.css('p.name a::text').extract_first()
-
-                # print(response.meta['category_big_name'], response.meta['category_small_name'], book_author, book_price_cn, book_name)
-                item['book_price_cn'] = book_price_cn
-                item['book_name'] = book_name
-                item['book_author'] = book_author
-                item['category_big_name'] = response.meta['category_big_name']
-                item['category_small_name'] = response.meta['category_small_name']
+                item['book_price_cn'] = book.css('p.price span.price_n::text').extract_first()
+                item['book_name'] = book.css('p.name a::text').extract_first()
+                item['book_author'] = book.css('p.author a::text').extract_first()
                 yield item
 
             next_page = response.css('li.next a::attr(href)').extract_first()
             if next_page is not None:
-                yield scrapy.Request(response.urljoin(next_page), callback=self.third_parse)
+                yield scrapy.Request(url=response.urljoin(next_page),
+                                     callback=self.third_parse,
+                                     meta={'item': item}, dont_filter=True)
 
 
 
